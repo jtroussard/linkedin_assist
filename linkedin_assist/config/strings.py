4154a68129084158d743cc805f08c307b5494b81
Jacques <jacques.troussard@gmail.com>
2020-01-18 09:16:22 -0500

    reload ignore file

--- linkedin_assist/config/strings.py
+++
@@ -1,6 +0,0 @@
-STRINGS = {
-	'manual_auth_prompt': 'Attention! You will be taken to a LinkedIn page to authenticate.\nEnter Linkedin user login credentials, accept, and then follow\nthese instructions.\n\nTo athenticate;\n   1. Copy the URL from the browser after signing into LinkedIn\n   2. Return to this terminal.\n   3. Paste the address in the prompt and press the ENTER key.\n\n                        [Press ENTER]\n',
-	'main_token_not_found': '\n============================================================\nOAuth2 token not found. User will have to authenticate.\n============================================================\n\n             Please follow the prompts.\n',
-	'main_token_present':'\n============================================================\nOAuth2 token present, will attempt to use token.\n============================================================\n\n',
-	
-	}
